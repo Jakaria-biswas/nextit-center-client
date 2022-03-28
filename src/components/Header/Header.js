78daa3990b57bf8961ccbfe796f7c1123d75a7d0
jakaria biswas <contactjakaria95@gmail.com>
2022-03-28 07:35:12 +0600

    navbar toggler update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,9 +14,9 @@ const Header = () => {
                 <div className='container  '  >
                     <nav class="navbar navbar-expand-lg navbar-light bg-light   "    >
                         <div class="container-fluid">
-                            <img class="navbar-brand" src={logo}></img>
+                            <img class="navbar-brand" src={logo} alt=""></img>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
+                                <span class="navbar-toggler-icon "></span>
                             </button>
 
                             <div class="collapse navbar-collapse " id="navbarSupportedContent">
